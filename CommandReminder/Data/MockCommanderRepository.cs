0c18735cf5db96ff883709d2e653a65383fdadf9
Fernando Giarola <fernandogiarola97@gmail.com>
2020-10-19 10:45:21 -0300

    feat: add Commander repository and Mock contract

---
+++ CommandReminder/Data/MockCommanderRepository.cs
@@ -0,0 +1,25 @@
+﻿using CommandReminder.Models;
+using System.Collections.Generic;
+
+namespace CommandReminder.Data
+{
+    public class MockCommanderRepository : ICommanderRepository
+    {
+        public IEnumerable<Command> GetAppCommands()
+        {
+            var commands = new List<Command>
+            {
+                new Command { Id = 0, HowTo = "Boil an egg", Line = "Boil Water", Platform = "Kettle & Pan" },
+                new Command { Id = 1, HowTo = "Cut bread", Line = "Get a knife", Platform = "knife & chopping board" },
+                new Command { Id = 2, HowTo = "Make a cup of tea", Line = "Place teabag in cup", Platform = "Kettle & cup" }
+            };
+
+            return commands;
+        }
+
+        public Command GetCommandById(int id)
+        {
+            return new Command { Id = 0, HowTo = "Boil an egg", Line = "Boil Water", Platform = "Kettle & Pan" };
+        }
+    }
+}
