94f5f01810cc64628f7b6eba422a41de3a0d2347
Fernando Giarola <fernandogiarola97@gmail.com>
2020-10-22 19:00:38 -0300

    feat: add data annotations to CommandCreateDto

--- CommandReminder/Dtos/CommandCreateDto.cs
+++ CommandReminder/Dtos/CommandCreateDto.cs
@@ -1,11 +1,17 @@
-﻿namespace CommandReminder.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CommandReminder.Dtos
 {
     public class CommandCreateDto
     {
+        [Required]
+        [MaxLength(100)]
         public string HowTo { get; set; }
 
+        [Required]
         public string Line { get; set; }
 
+        [Required]
         public string Platform { get; set; }
     }
 }
